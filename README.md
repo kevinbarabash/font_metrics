8beada6b61376d421afef86c05e1e0fe171a3767
Kevin Barabash <kevinb7@gmail.com>
2015-11-14 15:47:59 -0800

    rewrite in python

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
 # font_metrics
-Extract font metrics and display them in an web based viewer.
+Extract font metrics to a .json file
 
 ## Future work
-- display metrics in a zoomed in view on a single glyph
-- use react to make views easier to work with
 - extract outline data
-- display bezier curves and control points
-- allow users to modify the curves
---
+++ font_metrics.py
@@ -0,0 +1,52 @@
+import json
+
+from fontTools.ttLib import TTFont
+
+font_info = TTFont("Comic Sans MS.ttf")
+
+basic_latin = range(0x0020, 0x007F)
+latin_1 = range(0x0080, 0x00FF)
+greek = range(0x0370, 0x03FF)
+math_operators = range(0x2200, 0x22FF)
+
+char_list = basic_latin + latin_1 + greek + math_operators
+
+cmap = {}
+
+for t in font_info['cmap'].tables:
+    for k, v in t.cmap.iteritems():
+        if k in char_list:
+            cmap[k] = v
+
+        if k in cmap and cmap[k] != v:
+            print "%s: %s != %s" % (k, cmap[k], v)
+
+glyf = font_info["glyf"]
+hmtx = font_info["hmtx"]
+
+glyph_metrics = {}
+
+for k, v in cmap.iteritems():
+    glyph = glyf[v]
+    [advance, _] = hmtx[v]
+
+    xMin = getattr(glyph, "xMin", 0)
+    yMin = getattr(glyph, "yMin", 0)
+    xMax = getattr(glyph, "xMax", 0)
+    yMax = getattr(glyph, "yMax", 0)
+
+    bearingX = xMin
+    bearingY = yMin
+    width = xMax - xMin
+    height = yMax - yMin
+
+    glyph_metrics[k] = {
+        "advance": advance,
+        "bearingX": bearingX,
+        "bearingY": bearingY,
+        "width": width,
+        "height": height
+    }
+
+print json.dumps(glyph_metrics, indent=4, sort_keys=True)
+
